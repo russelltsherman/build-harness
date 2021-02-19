c4211c09b8f6525ecd4ba154f0f48e8fd5e28279
Russell <russell.t.sherman@gmail.com>
2021-02-19 09:45:21 -0600

    more robust lib git

--- lib/git.sh
+++ lib/git.sh
@@ -1,15 +1,32 @@
 #!/usr/bin/env bash
 
+git_update_branch() {
+  local branch="$1"
+  set +e
+  if git branch -a | egrep "remotes/origin/$branch"
+  then
+    action "update $branch branch"
+    git checkout "$branch" && \
+    git pull origin "$branch" && \
+    git submodule update --recursive #> /dev/null 2>&1
+  fi
+  set -e
+}
+
 git_clone_or_update() {
-  if [ -d "$2/.git" ]; then
-    action "update $1 master branch"
-    ( cd "$2" || exit; git checkout master && git pull origin master && git submodule update --recursive > /dev/null 2>&1 )
-    if git branch -a | egrep 'remotes/origin/develop'; then
-      action "update $1 develop branch"
-      ( cd "$2" || exit; git checkout develop && git pull origin develop && git submodule update --recursive > /dev/null 2>&1 )
-    fi
+  local url="$1"
+  local directory="$2"
+
+  if [ -d "$directory/.git" ]
+  then
+    ( 
+      cd "$directory" || exit; 
+      git_update_branch "main"
+      git_update_branch "master"
+      git_update_branch "develop"
+    )
   else
     action "clone $1"
-    git clone --recurse-submodules "$1" "$2" > /dev/null 2>&1
+    git clone --recurse-submodules "$url" "$directory" #> /dev/null 2>&1
   fi
 }
